24d4bfc464b12d28b5045cfa493bda6e14460388
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-06-12 10:23:54 +0200

    removed < from dictionary

--- partials/variables.php
+++ partials/variables.php
@@ -4,7 +4,7 @@
     $dictionaryAll = [
         'letter' => array_merge(range('a','z'), range('A', 'Z')),
         'number' => array_merge(range(0,9)),
-        'special' => array_merge(range(':','@'),range('`',']'))
+        'special' => array_merge(range('=','@'),range('`',']'))
     ];
 
 ?>
